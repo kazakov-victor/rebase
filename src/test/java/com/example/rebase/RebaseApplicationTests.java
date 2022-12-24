af41299aedb00983ccef8b98b35e1a1d88fcbaa5
kazakov-victor <vmailua@meta.ua>
2022-12-24 15:03:34 +0200

    Init commit

---
+++ src/test/java/com/example/rebase/RebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
